e39b90864c80d782ce590308709647da9389a177
Deepen Dhulla <deepen@deependhulla.com>
2019-12-27 14:26:22 +0530

    update 2019-12-27

--- primary/work-of-pusing-data-to-dr.sh
+++ primary/work-of-pusing-data-to-dr.sh
@@ -3,6 +3,7 @@ cd /usr/local/src/ceph-dr-sync-tool-for-proxmox
 
 # copy the cofig once
 #scp /etc/pve/nodes/pve01/qemu-server/100.conf root@172.16.201.74:/etc/pve/nodes/pve02/qemu-server/
+#scp /etc/pve/nodes/pve01/qemu-server/101.conf root@172.16.201.74:/etc/pve/nodes/pve02/qemu-server/
 
 php sync-primary-to-dr-site.php vm-100-disk-0
 php sync-primary-to-dr-site.php vm-100-disk-1
