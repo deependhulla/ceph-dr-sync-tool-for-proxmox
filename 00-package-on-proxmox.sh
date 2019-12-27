5c3cba9efc58b50369bdde39ad8e635cd523de92
Deepen Dhulla <deepen@deependhulla.com>
2019-12-27 14:43:47 +0530

    update 2019-12-27

--- 00-package-on-proxmox.sh
+++ 00-package-on-proxmox.sh
@@ -1,1 +1,9 @@
 apt-get install vim php-cli mc screen iptraf git 
+
+# from primary to ssh
+# example primary : 172.16.201.25
+# dr : 172.16.201.74
+
+## needed for autologin to primary to login to DR for all scripts
+#ssh-copy-id root@172.16.201.74
+
